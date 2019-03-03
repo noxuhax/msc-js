022653fdfd91bef9fbd16afb919cb30ee7cb752f
Sasha Averin <noxuhax@setka.io>
2019-03-03 19:49:02 +0300

    Lesson 5: Math

---
+++ 18_math/js/script.js
@@ -0,0 +1,15 @@
+console.log("it works!");
+
+function moduleHalf(num) {
+  return Math.abs(num)/2;
+}
+
+console.log(moduleHalf(-10));
+console.log(moduleHalf(4));
+
+console.log(Math.floor(2.5678));
+console.log(Math.round(2.5678));
+
+console.log(Math.random());
+console.log(Math.random() * (10 - 5) + 5);
+console.log(Math.floor(Math.random() * (10 - 5) + 5));
