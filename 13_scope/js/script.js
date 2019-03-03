f3069a8d03df3f70d2de4f2066aaa3c85784c2be
Sasha Averin <noxuhax@setka.io>
2019-03-03 17:38:16 +0300

    Lesson 4: scope

---
+++ 13_scope/js/script.js
@@ -0,0 +1,20 @@
+console.log("it works!");
+
+var a = "ololo";
+
+function myFunc() {
+  a = "wow";
+  return a;
+}
+
+console.log(a);
+console.log(myFunc());
+
+
+if (true) {
+  var b = "n2";
+  let c = "niBBa";
+}
+
+console.log(b);
+console.log(c); // nope!
