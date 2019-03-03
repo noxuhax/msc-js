a6230aa16102453d476414d12ad4a5eb771d3ae9
Sasha Averin <noxuhax@setka.io>
2019-03-03 16:16:46 +0300

    Lesson 4: Funtion

---
+++ 10_function/js/script.js
@@ -0,0 +1,15 @@
+console.log("it works!");
+
+function sayHi() {
+  return 'hi!';
+}
+
+// document.body.innerHTML = sayHi();
+console.log(sayHi());
+
+function sayHiToMe(name) {
+  return 'hi, ' + name;
+}
+
+console.log(sayHiToMe('Sasha'));
+console.log(sayHiToMe('Egor'));
