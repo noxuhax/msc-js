eca06054bbb6871c293718273fc2bceb3e163be8
Sasha Averin <noxuhax@setka.io>
2019-03-03 20:14:50 +0300

    Lesson 5: objects

---
+++ 20_array/js/script.js
@@ -0,0 +1,24 @@
+console.log("it works!");
+
+let people = [
+  "frank",
+  "josh",
+  "helen"
+];
+
+console.log(people);
+console.log(people[0]);
+
+people[0] = "phrank";
+console.log(people[0]);
+
+console.log(people.length);
+
+
+let bigArray = [
+  ['asd','qwe','zxc'],
+  [1,2,3],
+  [true,false]
+];
+console.log(bigArray[0]);
+console.log(bigArray[0][2]);
