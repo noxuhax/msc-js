38f7bef014c56a3803743998a60215e40fd3d450
Sasha Averin <noxuhax@setka.io>
2019-03-10 14:51:47 +0300

    Lesson 5: For each

---
+++ 27_foreach/js/script.js
@@ -0,0 +1,10 @@
+console.log("it works!");
+
+const button = document.querySelector('button');
+const lorems = document.querySelectorAll('div');
+
+button.onclick = () => {
+  lorems.forEach(lorem => {
+    lorem.style.fontSize = '30px';
+  })
+}
